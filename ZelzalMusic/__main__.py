12a15462f64a1f677d0103865fa7ac58e48f7d4d
sir-ahmed <167148201+sir-ahmed@users.noreply.github.com>
2024-06-07 23:23:58 +0300

    Update __main__.py

--- ZelzalMusic/__main__.py
+++ ZelzalMusic/__main__.py
@@ -9,8 +9,7 @@ import config
 from ZelzalMusic import LOGGER, app, userbot
 from ZelzalMusic.core.call import Zelzaly
 from ZelzalMusic.misc import sudo
-from ZelzalMusic.plugins import ALL_MODULES
-from ZelzalMusic.plugins.play.azan import azkar
+from ZelzalMusic.plugins import ALL_MODULES 
 from ZelzalMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
@@ -54,7 +53,6 @@ async def init():
     LOGGER("ميــوزك دارك").info(
         "\x44\x6f\x6e\x65\x20\x69\x6e\x73\x74\x61\x6c\x6c\x61\x74\x69\x6f\x6e\x20\x5a\x54\x68\x6f\x6e\x20\x4d\x75\x73\x69\x63\x20\xe2\x9c\x93\x0a\x47\x6f\x20\x74\x6f\x20\x79\x6f\x75\x72\x20\x62\x6f\x74\x20\x61\x6e\x64\x20\x73\x65\x6e\x64\x20\x2f\x73\x74\x61\x72\x74\x0a\x43\x68\x61\x6e\x6e\x65\x6c\x20\x3a\x20\x28\x40\x5a\x54\x68\x6f\x6e\x29"
     )
-    await azkar()
     await idle()
     await app.stop()
     await userbot.stop()
