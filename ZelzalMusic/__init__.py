d5279a4ec07b6211126c3c972284da5f86dd40e1
sir-ahmed <167148201+sir-ahmed@users.noreply.github.com>
2024-06-07 11:18:44 +0300

    Update __init__.py

--- ZelzalMusic/__init__.py
+++ ZelzalMusic/__init__.py
@@ -1,14 +1,12 @@
 
 from ZelzalMusic.core.bot import Zelzaly
 from ZelzalMusic.core.dir import dirr
-from ZelzalMusic.core.git import git
 from ZelzalMusic.core.userbot import Userbot
 from ZelzalMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
 dirr()
-git()
 dbb()
 heroku()
 
